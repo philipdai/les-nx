260327aabadee15937f92a4db2ef97fb6c3cbc71
Cen Dai <cendai@Cens-MBP.attlocal.net>
2020-02-16 08:57:40 -0500

    Using apis in componnent

--- myworkspace/apps/todos/src/app/app.module.ts
+++ myworkspace/apps/todos/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
   providers: [],
