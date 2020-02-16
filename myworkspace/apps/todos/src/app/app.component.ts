ea3391d2ce76d2b8a16bc98d814432e977e19e95
Cen Dai <cendai@Cens-MBP.attlocal.net>
2020-02-16 09:44:51 -0500

    Using shared model

--- myworkspace/apps/todos/src/app/app.component.ts
+++ myworkspace/apps/todos/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-interface Todo {
-  title: string;
-}
+import { Todo } from '@myworkspace/data';
 
 @Component({
   selector: 'myworkspace-root',
