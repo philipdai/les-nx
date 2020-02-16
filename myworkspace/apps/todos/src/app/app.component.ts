984c8873aa654d13ebde9d7d2b3ae0b6a18b8188
Cen Dai <cendai@Cens-MBP.attlocal.net>
2020-02-16 08:50:18 -0500

    Initial commit

---
+++ myworkspace/apps/todos/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+interface Todo {
+  title: string;
+}
+
+@Component({
+  selector: 'myworkspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  todos: Todo[] = [
+    { title: 'Todo 1'},
+    { title: 'Todo 2'}
+  ];
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`
+    });
+  }
+}
